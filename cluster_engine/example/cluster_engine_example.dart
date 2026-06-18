05e2f0e341e0bf12f8198d2ac46e185de6b5d31c
rafinha <rafinha72465@gmail.com>
2026-06-17 21:36:21 -0300

    feat(ce): criação das primeiras entidades e organização da arquitetura

--- cluster_engine/example/cluster_engine_example.dart
+++ cluster_engine/example/cluster_engine_example.dart
@@ -1,6 +1,4 @@
 import 'package:cluster_engine/cluster_engine.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
 }
--- cluster_engine/test/cluster_engine_test.dart
+++ cluster_engine/test/cluster_engine_test.dart
@@ -2,15 +2,4 @@ import 'package:cluster_engine/cluster_engine.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
 }
